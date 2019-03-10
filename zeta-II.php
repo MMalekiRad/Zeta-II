01542b1f86436b545269dbd0b2ebad1ccd1da844
MMalekiRad <apkandroid22@gmail.com>
2019-03-10 18:04:34 +0330

    DeBug

--- zeta-II.php
+++ zeta-II.php
@@ -8,16 +8,17 @@
 
 # Remove BackEnd WP Tags.
 // remove junk from head
-remove_action('wp_head', 'feed_links', 2);
+remove_action('wp_head', 'rsd_link'); //removes EditURI/RSD (Really Simple Discovery) link.
+remove_action('wp_head', 'wlwmanifest_link'); //removes wlwmanifest (Windows Live Writer) link.
+remove_action('wp_head', 'wp_generator'); //removes meta name generator.
+remove_action('wp_head', 'wp_shortlink_wp_head'); //removes shortlink.
+remove_action( 'wp_head', 'feed_links', 2 ); //removes feed links.
+remove_action('wp_head', 'feed_links_extra', 3 );  //removes comments feed.
+remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
 remove_action('wp_head', 'index_rel_link');
-remove_action('wp_head', 'wlwmanifest_link');
-remove_action('wp_head', 'feed_links_extra', 3);
 remove_action('wp_head', 'start_post_rel_link', 10);
 remove_action('wp_head', 'parent_post_rel_link', 10);
 remove_action('wp_head', 'adjacent_posts_rel_link', 10);
-remove_action('wp_head', 'rsd_link');
-remove_action('wp_head', 'wp_shortlink_wp_head');
-remove_action('wp_head', 'wp_generator');
 //Resource Hints is a smart feature added to WordPress version 4.6. I think it might improve your site speed.
 // But if you want to disable it, try this:
 remove_action('wp_head', 'wp_resource_hints', 2);
@@ -31,6 +32,7 @@ remove_action('wp_head', 'wp_oembed_add_discovery_links');
 remove_action('template_redirect', 'rest_output_link_header', 11);
 
 add_filter('the_generator', '__return_empty_string');
+add_filter('xmlrpc_enabled', '__return_false');
 
 // remove version from scripts and styles
 function shapeSpace_remove_version_scripts_styles($src)
